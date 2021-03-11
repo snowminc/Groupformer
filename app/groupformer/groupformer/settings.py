e3de1f3c3ada307baf621137dd2ecdcdeae696ce
kristianmischke <kristianmischke@gmail.com>
2021-03-11 17:59:08 -0500

    finish merging changes (fix urls & settings)

--- app/groupformer/groupformer/settings.py
+++ app/groupformer/groupformer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'form',
     'project_input_test.apps.ProjectInputTestConfig',
     'django.contrib.admin',
     'django.contrib.auth',
