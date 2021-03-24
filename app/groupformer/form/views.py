01f3cadd6b28984fac14c031c254cd0b6b232d03
kristianmischke <kristianmischke@gmail.com>
2021-03-23 23:35:56 -0400

    Using django template and session to store cached temporary data while the instructor makes edits on the setup screen. rn very basic (just storing blank project data dict)

--- app/groupformer/form/views.py
+++ app/groupformer/form/views.py
@@ -19,14 +19,18 @@ def index(request):
 
 
 def setup_screen(request):
+
+    if 'setup_projects' not in request.session: # doesn't exist, so add single blank project
+        request.session['setup_projects'] = [
+            {
+                "name": "",
+                "description": "",
+            },
+        ]
+
+
     context = {}
 
-    context["participants"] = [
-        {"name": "Sarah Nakhon"},
-        {"name": "Morgan Vanderhei"},
-        {"name": "Kyle Morgan"},
-        {"name": "Min Chon"},
-        {"name": "Kristian Mischke"}
-    ]
+    context["projects"] = request.session['setup_projects']
 
     return render(request, "form/setup_screen.html", context=context)
