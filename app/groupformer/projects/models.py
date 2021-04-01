1dae221b8c90cc1d45849e3b926827e031f174b1
snakhon1 <snakhon1@umbc.edu>
2021-04-01 17:00:54 -0400

    created a function to add a project into the database

--- app/groupformer/projects/models.py
+++ app/groupformer/projects/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     project_name = models.CharField(max_length=240, blank=False, null=False)
     project_description = models.TextField(blank=False, null=False)
 
-
+    # model function to add a project name and project description
     def add_project(name, description):
         proj = Project(project_name = name, project_description = description)
         proj.save()
