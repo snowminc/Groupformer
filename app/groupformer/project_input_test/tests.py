3968fad340cc2b957ce53032b0b58945bd95d6ae
Morgan <morganv2@umbc.edu>
2021-04-01 15:49:48 -0400

    Stubs of tests added

--- app/groupformer/project_input_test/tests.py
+++ app/groupformer/project_input_test/tests.py
@@ -25,3 +25,20 @@ class ProjectIndexViewTests(TestCase):
         response = self.client.get(reverse('project_input_test:index'))
         self.assertContains(response, project.project_name)
         self.assertContains(response, project.project_description)
+
+class DatabaseTests(TestCase):
+    def test_setup(self):
+        #Setup a basic structure
+        pass
+    
+    def test_adding(self):
+        #Add various additional entities and relationships
+        pass
+    
+    def test_removing(self):
+        #Remove different elements and test their outputs
+        pass
+    
+    def test_cleanup(self):
+        #Clear database by removing GroupFormers
+        pass
