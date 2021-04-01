404ba66dd173aa29e9dbf6dac20e92e22415ad39
Min Chon <snowminc@gmail.com>
2021-04-01 18:38:47 -0400

    requirements.txt includes selenium, and the groupformer_list app now has a test for checking if the groups actually show onto the screen when prompted

--- app/groupformer/min_iteration2/tests.py
+++ app/groupformer/min_iteration2/tests.py
@@ -21,3 +21,54 @@ class MinIteration2ResponseScreenTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "How comfortable are you with front-end?")
         self.assertContains(response, "How comfortable are you with back-end?")
+
+    def test_displays_all_groups(self):
+        """
+        If the page shows all of the arbitrary group inputs (with a temporary "backend") from views.py, then it passes
+        """
+        response = self.client.get(reverse('min_iteration2:groupformer_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Section W")
+        self.assertContains(response, "A, B, C")
+        self.assertContains(response, "1, 2, 3")
+        self.assertContains(response, "X, Y, Z")
+        self.assertContains(response, "Section A")
+        self.assertContains(response, "Q, A, Z")
+        self.assertContains(response, "G, M, E")
+        self.assertContains(response, "A, S, D, F")
+
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+class SeleniumGroupformerList(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/min_iteration2/groupformer_list'))
+        first_groupformer = self.selenium.find_element_by_id("groupformer1_submit")
+        second_groupformer = self.selenium.find_element_by_id("groupformer2_submit")
+        first_groupformer.click()
+        second_groupformer.click()
+        first_groups = self.selenium.find_element_by_id("groupformer1_groups")
+        second_groups = self.selenium.find_element_by_id("groupformer2_groups")
+
+        self.assertTrue("A, B, C" in first_groups.get_attribute("innerHTML"))
+        self.assertTrue("1, 2, 3" in first_groups.get_attribute("innerHTML"))
+        self.assertTrue("X, Y, Z" in first_groups.get_attribute("innerHTML"))
+
+        self.assertTrue("Q, A, Z" in second_groups.get_attribute("innerHTML"))
+        self.assertTrue("G, M, E" in second_groups.get_attribute("innerHTML"))
+        self.assertTrue("A, S, D, F" in second_groups.get_attribute("innerHTML"))
+
+        
