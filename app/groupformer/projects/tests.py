d0f6dbb97776de95aba4b6c0e088a69055fd1d13
snakhon1 <snakhon1@umbc.edu>
2021-04-01 17:00:06 -0400

    Several tests created to ensure that data is properly being saved into the database

--- app/groupformer/projects/tests.py
+++ app/groupformer/projects/tests.py
@@ -1,20 +1,31 @@
 from django.test import TestCase
-from .models import Project
+from django.urls import reverse
+from projects.models import Project
 
 class ProjectModelTests(TestCase):
-    def test_get_first_project(self):
-        project_obj = Project.objects.first()
-        self.assertEqual(project_obj.project_name, "Group Former")
+    #function to set up the test functions by adding Project Objects into the "test" database
+    def setUp(self):
+        Project.add_project("Test 1", "Test 1 Description")
+        Project.add_project("Test 2", "Test 2 Description")
+        Project.add_project("Test 3", "Test 3 Description")
+        Project.add_project("Test 4", "Test 4 Description")
 
-    # def test_get_last_project(self):
-    #     project_obj = Project.objects.last()
-    #     print('Project Name: ', project_obj.project_name, '\n')
-    #     print('Project Description: ', project_obj.project_description, '\n')
-    #
-    # def test_print_all_objects(self):
-    #     for project in Project.objects.all():
-    #         print('Project Name: ', project.project_name, '\n')
-    #         print('Project Description: ', project.project_description, '\n')
+    def test_first_project(self):
+        project_obj = Project.objects.all()[0]
+        self.assertEqual(project_obj.project_name, "Test 1")
+        self.assertEqual(project_obj.project_description, "Test 1 Description")
 
+    def test_last_project(self):
+        project_obj = Project.objects.last()
+        self.assertEqual(project_obj.project_name, "Test 4")
+        self.assertEqual(project_obj.project_description, "Test 4 Description")
+
+    def test_project_saved(self):
+        project_objects_dict = {"project_name": "Test 1", "project_description": "Test Description"}
+        response = self.client.post(reverse('add_project'), project_objects_dict)
+        # response code for redirecting is 302
+        self.assertEqual(response.status_code, 302)
+        project_obj = Project.objects.all()[0]
+        self.assertEqual(project_obj.project_name, "Test 1")
 
 
