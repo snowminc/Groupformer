320d47566d8fe69bb8b402bdb876e8599a585719
Morgan <morganv2@umbc.edu>
2021-04-01 18:04:11 -0400

    Moved database into separate app for generalizing

---
+++ app/groupformer/dbtools/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class DatabaseTests(TestCase):
+    def test_setup(self):
+        #Setup a basic structure
+        pass
+    
+    def test_adding(self):
+        #Add various additional entities and relationships
+        pass
+    
+    def test_removing(self):
+        #Remove different elements and test their outputs
+        pass
+    
+    def test_cleanup(self):
+        #Clear database by removing GroupFormers
+        pass
--- app/groupformer/groupformer/settings.py
+++ app/groupformer/groupformer/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'form.apps.FormConfig',
     'project_input_test.apps.ProjectInputTestConfig',
+    'dbtools.apps.DbtoolsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
