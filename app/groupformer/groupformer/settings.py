d0a5cbb5016bd943fb7fa9bb41dcac2a067fb788
Min Chon <snowminc@gmail.com>
2021-04-01 17:30:10 -0400

    Created a front end page for a sample response screen, using arbitrary sample 'project' and 'attribute' model data for proof of concept, as well as a instructor-side page for showing groups (WIP)

--- app/groupformer/groupformer/settings.py
+++ app/groupformer/groupformer/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'form.apps.FormConfig',
     'project_input_test.apps.ProjectInputTestConfig',
+    'min_iteration2.apps.MinIteration2Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
