eeabd9d0a785894e0f1ba0d48273fab8ec237446
snakhon1 <snakhon1@umbc.edu>
2021-04-01 17:14:05 -0400

    Added a url pattern for the project app that uses the urls in the project directory

--- app/groupformer/groupformer/urls.py
+++ app/groupformer/groupformer/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
 urlpatterns = [
     path('project_input_test/', include('project_input_test.urls')),
     path('temp_form/', include('form.urls')),
     path('admin/', admin.site.urls),
+    path('projects/', include('projects.urls'))
 ]
