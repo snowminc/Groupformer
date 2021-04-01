af98939286c6af96bb65be337e3a48c660aabced
snakhon1 <snakhon1@umbc.edu>
2021-04-01 16:59:12 -0400

    Created 2 views - one for the form to be rendered and another for the form to send the data it received

--- app/groupformer/projects/views.py
+++ app/groupformer/projects/views.py
@@ -1,6 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from .models import Project
 # Create your views here.
+
+# view to render/display the form
+def project_index(request):
+    return render(request, "projects/projects.html")
+
+# view where the form sends the data, accepts the project names and descriptions
 def project_create_view(request):
-    context = {}
-    return render(request, "projects/projects.html", context)
+    """
+    :param request:
+    :return:
+    """
+    # assuming the front_end is saving what the users enters as project_name and project_description
+    Project.add_project(request.POST["project_name"], request.POST["project_description"])
+    #allows it to go back to ask for another project description - index is displaying the page
+    #reverse takes the name and looks in urls for the full urls
+    #sending them back to the form -> project_index to stay on the same page to ask for another
+    # project name and description
+    return HttpResponseRedirect(reverse("project_index"))
