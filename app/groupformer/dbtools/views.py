1ecd149e63296f4eac6d29060abd6ac2ae4198d4
snakhon1 <snakhon1@umbc.edu>
2021-04-12 12:21:28 -0400

    Added the view to verify the participant

--- app/groupformer/dbtools/views.py
+++ app/groupformer/dbtools/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import GroupFormer, Participant
+
+def verify_participant(request, group_former_id):
+    group_former = get_object_or_404(GroupFormer, pk=group_former_id)
+    try:
+     particpant_obj = group_former.participant_set.filter(part_email = request.GET['email'])
+    except(KeyError, Participant.DoesNotExist):
+        return render(request, 'dbtools/pdenied.html')
+    return HttpResponseRedirect(reverse('min_iteration2:response_screen'))
+
+
+
