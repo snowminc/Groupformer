1e0d7c82b17df36eefa5a09032a90cba01826471
Morgan <morganv2@umbc.edu>
2021-04-14 16:53:45 -0400

    Removed projects/ directory
    Merge is completed

--- app/groupformer/projects/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register the Project Model on the admin site
-from .models import Project
-
-admin.site.register(Project)
--- app/groupformer/projects/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-#url pattern for displaying the form
-#url pattern for saving what is contained in the form
-urlpatterns = [
-    path('', views.project_index, name='project_index'),
-    path('add_project', views.project_create_view, name='add_project'),
-]
