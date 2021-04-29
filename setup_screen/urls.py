133159e85cacb5a07d692b4babdf190ec4289e87
kristianmischke <kristianmischke@gmail.com>
2021-04-29 10:34:52 -0400

    Adding distinction between login and create account

--- setup_screen/urls.py
+++ setup_screen/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('submit_groupformer', views.submit_groupformer, name='submit_groupformer'),
     path('login_screen', views.login_screen, name='login_screen'),
+    path('create_account_screen', views.create_account_screen, name='create_account_screen'),
     path('login_endpoint', views.login_endpoint, name='login_endpoint'),
     path('logout_endpoint', views.logout_endpoint, name='logout_endpoint'),
-    path('create_account', views.create_account, name='create_account'),
+    path('create_account', views.create_account, name='create_account_endpoint'),
 ]
--- setup_screen/views.py
+++ setup_screen/views.py
@@ -71,7 +71,18 @@ def login_screen(request):
         return redirect(reverse('setup_screen:index'))
 
     # display login screen
-    return render(request, 'setup_screen/instructor_login.html')
+    return render(request, 'setup_screen/instructor_login.html', context={'create_account': False})
+
+
+def create_account_screen(request):
+
+    if request.user.is_authenticated:
+        if 'results_screen' in request:  # TODO: test & implement proper redirecting
+            return redirect(reverse('results_screen:results_screen'))
+        return redirect(reverse('setup_screen:index'))
+
+    # display login screen
+    return render(request, 'setup_screen/instructor_login.html', context={'create_account': True})
 
 
 def login_endpoint(request):
@@ -83,25 +94,40 @@ def login_endpoint(request):
         login(request, user)
         if 'results_screen' in request:  # TODO: test & implement proper redirecting
             return redirect(reverse('results_screen:results_screen'))
-        return redirect(reverse('setup_screen::index'))
+        return redirect(reverse('setup_screen:index'))
 
     # failed to authenticate, so display error message
-    return render(request, 'setup_screen/instructor_login.html', {'error': 'could not authenticate user'})
+    return render(request, 'setup_screen/instructor_login.html', {'error': 'could not authenticate user', 'create_account': False})
 
 
 def create_account(request):
+    username = request.POST['username']
     email = request.POST['email']
     password = request.POST['password']
+    confirm_password = request.POST['confirm_password']
 
-    user: User = User.objects.create_user(username=email.split('@')[0], email=email, password=password)
+    if password != confirm_password:
+        # TODO: create account should not render a new page (b/c we want to preserve the user's input)
+        # TODO: should be an ajax request instead
+        return render(request, 'setup_screen/instructor_login.html',
+                      {'error': 'Passwords do not match!', 'create_account': True})
+
+    if len(User.objects.filter(username=username)) > 0:
+        # TODO: create account should not render a new page (b/c we want to preserve the user's input)
+        # TODO: should be an ajax request instead
+        return render(request, 'setup_screen/instructor_login.html',
+                      {'error': 'Username already taken', 'create_account': True})
+
+    user: User = User.objects.create_user(username=username, email=email, password=password)
     user.first_name = request.POST['first_name']
     user.last_name = request.POST['last_name']
+    user.save()
 
     # redirect to setup screen
-    return redirect(reverse('setup_screen::index'))
+    return redirect(reverse('setup_screen:login_screen'))
 
 
 def logout_endpoint(request):
     logout(request)
 
-    return render(request, 'setup_screen/instructor_login.html', {'success': 'Logged out successfully!'})
+    return render(request, 'setup_screen/instructor_login.html', {'success': 'Logged out successfully!', 'create_account': False})
