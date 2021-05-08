06cb70e29792dfe27b4da72d09c5b816e1426568
kristianmischke <kristianmischke@gmail.com>
2021-05-08 17:05:43 -0400

    Updated homepage tests. Added exception to prio alg for max_parts
    using max participants in get_groups results screen view

--- results_screen/views.py
+++ results_screen/views.py
@@ -30,7 +30,7 @@ def results_screen(request):
 
 def get_groups(request, groupformer_id):
     groupformer = GroupFormer.objects.get(pk=groupformer_id)
-    best_groups, second_groups, third_groups = calc_optimal_groups(groupformer, max_parts=3, epoch=400)
+    best_groups, second_groups, third_groups = calc_optimal_groups(groupformer, max_parts=groupformer.max_participants_per_group, epoch=400)
     
     payload = {}
     for group in best_groups[0]:
